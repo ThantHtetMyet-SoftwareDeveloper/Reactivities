60f6fe32c5156fbccc69ee908bfa6862983bda00
Aye Moe Phyu <ayemoephyu151996@gmail.com>
2021-06-07 16:19:21 +0800

    End of Session-4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,32 @@
+using MediatR;
+using System.Collections.Generic;
+using Domain;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> {}
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                // throw new System.NotImplementedException();
+                return await _context.Activities.ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
